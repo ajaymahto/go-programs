833d7f0addbd26af8509a0292b1ce7073023505d
ajay.mahto <ajay.mahto@grabtaxi.com>
2020-04-10 23:06:12 +0530

    Adding variables.go

---
+++ variables.go
@@ -0,0 +1,32 @@
+// Author: Ajay Kumar Mahto <aj.mahto01@gmail.com>
+// Description: Playing with variables in golang.
+
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var s string
+	fmt.Println(s)
+
+	var g float32
+	fmt.Println(g)
+
+	var boo bool
+	fmt.Println(boo)
+
+	f := "apple"
+	fmt.Println(f)
+}
