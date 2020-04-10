033b41a158a28247245a57a515715727b6a65985
ajay.mahto <ajay.mahto@grabtaxi.com>
2020-04-10 22:53:31 +0530

    Adding values.go

---
+++ values.go
@@ -0,0 +1,16 @@
+// Author: Ajay Kumar Mahto <aj.mahto01@gmail.com>
+// Description: Printing values
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7/3 =", 7/3)
+	fmt.Printf("7.0/3 = %0.2f\n", 7.0/3)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
